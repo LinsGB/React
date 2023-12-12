1b9b681bc737e9f24aa01f7a0f6533cbb43b053f
Gabriel Lins <linsgb2@gmail.com>
2023-12-12 15:38:29 -0300

    feat - adding react-hooks project

---
+++ react-hooks/src/App.js
@@ -0,0 +1,12 @@
+import { WithCallback, WithoutCallback } from './hooks_examples/callback';
+import { WithReducer } from './hooks_examples/reducer';
+
+function App() {
+
+
+  return (
+    <WithCallback/>
+  );
+}
+
+export default App;
